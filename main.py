a6eaa1bdc845cd0e0cfe861427915bc22a03ba42
Anzhalika Turlak <293852647@mail.ru>
2022-08-24 20:43:09 +0300

    lesson 5 done

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+# from tracker import *
+#
+# import sys
+#
+# app = QtWidgets.QApplication(sys.argv)
+# MainWindow = QtWidgets.QMainWindow()
+# ui = Ui_MainWindow()
+# ui.setupUi(MainWindow)
+# MainWindow.show()
+#
+# ui.label.setText('erthtzrjnsymzgs')
+#
+# sys.exit(app.exec())
+
+
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
 
@@ -10,4 +25,18 @@ window = Window()
 form = Form()
 form.setupUi(window)
 window.show()
+
+def on_click():
+    print(form.plainTextEdit.toPlainText())
+    print(form.dateEdit.dateTime().toString('dd-MM-yyyy'))
+    print('Cliked!!!')
+
+form.pushButton.clicked.connect(on_click)
+
+
+#form.label.setText('erthtzrjnsymzgs')
+
+
 app.exec_()
+
+
